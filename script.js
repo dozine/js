57b4dbc7b07a78706885d5687e9acdf677258a41
Dozin Jang <108924060+dozine@users.noreply.github.com>
2023-05-18 18:54:25 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const timeDiv = document.getElementById("time");
+
+function getTime() {
+  let now = new Date();
+  let year = padZero(now.getFullYear());
+  let month = padZero(now.getMonth());
+  let day = padZero(now.getDay());
+  let hour = padZero(now.getHours());
+  let minute = padZero(now.getMinutes());
+  let second = padZero(now.getSeconds());
+
+  timeDiv.textContent = `${hour}시 ${minute}분 ${second}초`;
+
+  setInterval(getTime, 1000);
+}
+
+function padZero(data) {
+  return data < 10 ? `0${data}` : data;
+}
+
+getTime();
